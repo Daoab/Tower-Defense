6664f22af1b1404279cb11115fc909d0396315dc
Daoab <38359000+Daoab@users.noreply.github.com>
2018-09-19 11:45:19 +0200

    Initial Commit

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Tooltip("Adjust the snapping distance")][Range(1f,20f)] float gridSize = 10;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; // rounds the x coordinate of the gameobjecto and the sets it to the correct scale
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+    }
+}
