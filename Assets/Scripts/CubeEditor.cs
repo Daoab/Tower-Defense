514f45fdd6813312dd5a4631081b062b79741436
Daoab <38359000+Daoab@users.noreply.github.com>
2018-09-19 12:18:00 +0200

    Implemented coordinate system

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Tooltip("Adjust the snapping distance")][Range(1f,20f)] float gridSize = 10;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
 
     void Update()
     {
@@ -13,5 +21,7 @@ public class EditorSnap : MonoBehaviour
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; // rounds the x coordinate of the gameobjecto and the sets it to the correct scale
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+
+        textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
     }
 }
